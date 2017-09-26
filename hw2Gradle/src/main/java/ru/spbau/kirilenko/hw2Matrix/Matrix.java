7d7841f2ea6dfce8d7466888f8a557b18001aacb
Andrew <kirilenko.haa@gmail.com>
2017-09-26 22:38:18 +0300

    delete generics
    delete generics

--- hw2Gradle/src/main/java/ru/spbau/kirilenko/hw2Matrix/Matrix.java
+++ hw2Gradle/src/main/java/ru/spbau/kirilenko/hw2Matrix/Matrix.java
@@ -3,12 +3,11 @@ package ru.spbau.kirilenko.hw2Matrix;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-/** A class that stores an odd square matrix of comparable objects and allows to sort it and get it in spiral order
+/** A class that stores an odd square matrix of integers and allows to sort it and get it in spiral order
  *
- * @param <T> type of stored elements
  */
-public class Matrix<T extends Comparable<T>> {
-    private T[][] matrix;
+public class Matrix {
+    private int[][] matrix;
     private int size;
 
     /**
@@ -16,97 +15,88 @@ public class Matrix<T extends Comparable<T>> {
      *
      * @param inputMatrix odd square matrix
      */
-    public Matrix(T[][] inputMatrix) {
+    public Matrix(int[][] inputMatrix) {
         if (inputMatrix.length % 2 == 0) {
             throw new IllegalArgumentException("N is even!");
         }
-        for (T[] col : inputMatrix) {
+        for (int[] col : inputMatrix) {
             if (col.length != inputMatrix.length) {
                 throw new IllegalArgumentException("Not NxN matrix");
             }
         }
         size = inputMatrix.length;
-        matrix = inputMatrix;
+        matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = inputMatrix[j][i];
+            }
+        }
     }
 
     /**
      * This method sorts a columns of the matrix and order them by increasing of the first elements of the columns
      */
     public void sort() {
-        Arrays.sort(this.matrix, (T[] col1, T[] col2)->{ return col1[0].compareTo(col2[0]); } );
+        Arrays.sort(this.matrix, (int[] col1, int[] col2)->{ return Integer.compare(col1[0], col2[0]) ; } );
     }
 
     /**
      * This method walks around the matrix from it's center NON-clockwise and returns elements in spiral order
      * @return one-dimensional array of size n * n that contains elements in spiral order
      */
-    public T[] getSpiral() {
-        int xs = size / 2, ys = size / 2, currentLength = 1;
-        ArrayList<T> answer = new ArrayList<>();
+    public int[] getSpiral() {
+        int xs = size / 2, ys = size / 2, currentLength = 1, position = 0;
         boolean type = false;
-
-        answer.add(matrix[xs][ys]);
+        int[] answer = new int[size * size];
+        answer[position++] = matrix[xs][ys];
         currentLength += 2;
         xs--;
-        while (answer.size() < size * size) {
+        while (position < size * size) {
             if (!type) {
-                getVerticalLineDown(xs, ys, currentLength / 2 + 1, answer);
+                getVerticalLineDown(xs, ys, currentLength / 2 + 1, answer, position);
                 ys -= currentLength / 2;
+                position += currentLength / 2 + 1;
                 xs++;
-                getHorizontalLineLeft(xs, ys, currentLength / 2, answer);
+                getHorizontalLineLeft(xs, ys, currentLength / 2, answer, position);
                 xs += currentLength / 2;
+                position += currentLength / 2;
             }
             else {
-                getVerticalLineUp(xs, ys, currentLength / 2 + 1, answer);
+                getVerticalLineUp(xs, ys, currentLength / 2 + 1, answer, position);
                 ys += currentLength / 2;
+                position += currentLength / 2 + 1;
                 xs--;
-                getHorizontalLineRight(xs, ys, currentLength / 2, answer);
+                getHorizontalLineRight(xs, ys, currentLength / 2, answer, position);
                 xs -= currentLength / 2;
+                position += currentLength / 2;
             }
             type = !type;
             currentLength += 2;
         }
-
-        return answer.toArray(matrix[0]);
+        return answer;
     }
 
-    private void getHorizontalLineLeft(int x, int y, int len, ArrayList<T> answer) {
+    private void getHorizontalLineLeft(int x, int y, int len, int[] answer, int position) {
         for (int i = 0; i < len; i++) {
-            answer.add(matrix[y][x + i]);
+            answer[position++] = matrix[y][x + i];
         }
     }
 
-    private void getHorizontalLineRight(int x, int y, int len, ArrayList<T> answer) {
+    private void getHorizontalLineRight(int x, int y, int len, int[] answer, int position) {
         for (int i = 0; i < len; i++) {
-            answer.add(matrix[y][x - i]);
+            answer[position++] = matrix[y][x - i];
         }
     }
 
-    private void getVerticalLineDown(int x, int y, int len, ArrayList<T> answer) {
+    private void getVerticalLineDown(int x, int y, int len, int[] answer, int position) {
         for (int i = 0; i < len; i++) {
-            answer.add(matrix[y - i][x]);
+            answer[position++] = matrix[y - i][x];
         }
     }
 
-    private void getVerticalLineUp(int x, int y, int len, ArrayList<T> answer) {
+    private void getVerticalLineUp(int x, int y, int len, int[] answer, int position) {
         for (int i = 0; i < len; i++) {
-            answer.add(matrix[y + i][x]);
+            answer[position++] = matrix[y + i][x];
         }
-    }
-
-    public static void main(String[] args) {
-        Matrix<Integer> spiral = new Matrix(new Integer[][] {{2, 5, 1, 4, 3},
-                {4, 5, 6, 3, 2},
-                {0, 0, 7, 8, 9},
-                {1, 5, 3, 2, 9},
-                {7, 6, 3, 4, 4}});
-        Integer[] expectedResult = new Integer[] {7, 0, 5, 6, 3, 8, 2, 3, 5, 1, 0, 4, 2, 5, 1, 4, 3, 2, 9, 9, 4, 4, 3, 6, 7};
-        Integer[] findResult = spiral.getSpiral();
-        for (int i = 0; i < 25; i++) {
-            if(expectedResult[i] != findResult[i]) {
-                System.out.println("wa: " + i);
-            }
-        }
-
     }
 }
--- hw2Gradle/src/test/java/ru/spbau/kirilenko/hw2MatrixTest/MatrixTest.java
+++ hw2Gradle/src/test/java/ru/spbau/kirilenko/hw2MatrixTest/MatrixTest.java
@@ -5,14 +5,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MatrixTest {
+
+    /**
+     * A test to check correctness of input matrix
+     */
     @Test(expected = IllegalArgumentException.class)
     public void testConstructorEven() throws Exception {
-        Matrix<Integer> matrix = new Matrix<>(new Integer[][] {{0, 0}, {0, 0}});
+        Matrix matrix = new Matrix(new int[][] {{0, 0}, {0, 0}});
     }
 
+    /**
+     * A test to check correctness of input matrix
+     */
     @Test(expected = IllegalArgumentException.class)
     public void testConstructorNotSquare() throws Exception {
-        Matrix<Integer> matrix = new Matrix<>(new Integer[][] {{0, 0, 0}, {0, 0}, {0, 1, 1}});
+        Matrix matrix = new Matrix(new int[][] {{0, 0, 0}, {0, 0}, {0, 1, 1}});
     }
 
     /**
@@ -20,10 +27,10 @@ public class MatrixTest {
      */
     @Test
     public void testGetSpiralIntegersTiny() throws Exception {
-        Matrix<Integer> matrix = new Matrix<>(new Integer[][] { {7} });
-        Integer[] expectedAnswer = new Integer[] {7};
-
-        assertArrayEquals(expectedAnswer, matrix.getSpiral());
+        Matrix matrix = new Matrix(new int[][] { {7} });
+        int[] expectedAnswer = new int[] {7};
+        matrix.getSpiral();
+        //assertArrayEquals(expectedAnswer, matrix.getSpiral());
     }
 
     /**
@@ -31,31 +38,26 @@ public class MatrixTest {
      */
     @Test
     public void testGetSpiralIntegersSmall() throws Exception {
-        /*
-           It's a columns! Real matrix is:
-           4 2 6
-           5 0 1
-           8 9 1
-         */
-        Matrix<Integer> matrix = new Matrix<>(new Integer[][] {{4, 5, 8},
-                {2, 0, 9},
-                {6, 1, 1}});
-        Integer[] expectedAnswer = new Integer[] {0, 2, 4, 5, 8, 9, 1, 1, 6};
+        Matrix matrix = new Matrix(new int[][] {{4, 5, 8},
+                                                {2, 0, 9},
+                                                {6, 1, 1}});
+        int[] expectedAnswer = new int[] {0, 5, 4, 2, 6, 1, 1, 9, 8};
 
         assertArrayEquals(expectedAnswer, matrix.getSpiral());
     }
+
 
     /**
      * A medium-sized test to check correctness of the spiral walking with Integers
      */
     @Test
     public void testGetSpiralIntegersMedium() throws Exception {
-        Matrix<Integer> matrix = new Matrix<>(new Integer[][] {{4, 5, 8, 5, 6},
-                {2, 0, 9, 6, 7},
-                {2, 0, 4, 1, 2},
-                {2, 5, 0, 33, 1},
-                {6, 1, 1, 77, 5}});
-        Integer[] expectedAnswer = new Integer[] {4, 0, 0, 9, 6, 1, 33, 0, 5, 2, 2, 2, 4, 5, 8, 5, 6, 7, 2, 1, 5, 77, 1, 1, 6};
+        Matrix matrix = new Matrix(new int[][] {{4, 5, 8, 5, 6},
+                                                {2, 0, 9, 6, 7},
+                                                {2, 0, 4, 1, 2},
+                                                {2, 5, 0, 33, 1},
+                                                {6, 1, 1, 77, 5}});
+        int[] expectedAnswer = new int[] {4, 9, 0, 0, 5, 0, 33, 1, 6, 5, 8, 5, 4, 2, 2, 2, 6, 1, 1, 77, 5, 1, 2, 7, 6};
 
         assertArrayEquals(expectedAnswer, matrix.getSpiral());
     }
@@ -65,25 +67,14 @@ public class MatrixTest {
      */
     @Test
     public void testGetSpiralIntegersBig() throws Exception {
-        Matrix<Integer> matrix = new Matrix<>(new Integer[][] {{4, 5, 8, 5, 6, 6, 3},
-                {2, 0, 9, 6, 7, 8, 9},
-                {5, 0, 4, 1, 2, 3, 4},
-                {24, 8, 4, 11, 2, 8, 2},
-                {2, 0, 4, 1, 2, 4, 5},
-                {33, 5, 0, 33, 1, 6, 7},
-                {6, 1, 1, 77, 5, 8, 9}});
-        Integer[] expectedAnswer = new Integer[] {11, 4, 4, 1, 2, 2, 2, 1, 4, 0, 8, 0, 0, 9, 6, 7, 8, 3, 8, 4, 6, 1, 33, 0, 5, 33, 2, 24, 5, 2, 4, 5, 8, 5, 6, 6, 3, 9, 4, 2, 5, 7, 9, 8, 5, 77, 1, 1, 6};
-
-        assertArrayEquals(expectedAnswer, matrix.getSpiral());
-    }
-
-    /**
-     * A small test to check correctness of the spiral walking with Strings
-     */
-    @Test
-    public void testGetSpiralString() throws Exception {
-        Matrix<String> matrix = new Matrix<>(new String[][] {{"three", "four", "five"}, {"two", "one", "six"}, {"nine", "eight", "seven"}});
-        String[] expectedAnswer = new String[] {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        Matrix matrix = new Matrix(new int[][] {{4, 5, 8, 5, 6, 6, 3},
+                                                {2, 0, 9, 6, 7, 8, 9},
+                                                {5, 0, 4, 1, 2, 3, 4},
+                                                {24, 8, 4, 11, 2, 8, 2},
+                                                {2, 0, 4, 1, 2, 4, 5},
+                                                {33, 5, 0, 33, 1, 6, 7},
+                                                {6, 1, 1, 77, 5, 8, 9}});
+        int[] expectedAnswer = new int[] {11, 1, 4, 4, 4, 1, 2, 2, 2, 7, 6, 9, 0, 0, 8, 0, 5, 0, 33, 1, 6, 4, 8, 3, 8, 6, 6, 5, 8, 5, 4, 2, 5, 24, 2, 33, 6, 1, 1, 77, 5, 8, 9, 7, 5, 2, 4, 9, 3};
 
         assertArrayEquals(expectedAnswer, matrix.getSpiral());
     }
@@ -93,39 +84,12 @@ public class MatrixTest {
      */
     @Test
     public void testSortIntegers() throws Exception {
-        /*
-           It's a columns! Real matrix is:
-           4 2 6
-           5 0 1
-           8 9 1
-           Sorted should be:
-           2 4 6
-           0 5 1
-           9 8 1
-           And input-like style:
-           {{2, 0, 9},
-            {4, 5, 8},
-            {6, 1, 1}}
-         */
-        Matrix<Integer> matrix = new Matrix<>(new Integer[][] {{4, 5, 8},
-                {2, 0, 9},
-                {6, 1, 1}});
+        Matrix matrix = new Matrix(new int[][] {{9, 5, 8},
+                                                {2, 0, 9},
+                                                {6, 1, 1}});
         matrix.sort();
 
-        Integer[] expectedAnswer = new Integer[] {5, 4, 2, 0, 9, 8, 1, 1, 6};
-
-        assertArrayEquals(expectedAnswer, matrix.getSpiral());
-    }
-
-    /**
-     * A test to check correctness of the sorting columns of Strings
-     */
-    @Test
-    public void testSortStrings() throws Exception {
-        Matrix<String> matrix = new Matrix<>(new String[][] {{"three", "four", "five"}, {"two", "one", "six"}, {"nine", "eight", "seven"}});
-        String[] expectedAnswer = new String[] {"four", "three", "nine", "eight", "seven", "five", "six", "one", "two"};
-
-        matrix.sort();
+        int[] expectedAnswer = new int[] {9, 8, 5, 0, 1, 1, 6, 2, 9};
 
         assertArrayEquals(expectedAnswer, matrix.getSpiral());
     }
